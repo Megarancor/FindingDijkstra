b30aaac0a180d133f56acd7b640b4258b1b33f61
David García-Brazales Santiago <davidbrazales@gmail.com>
2019-02-08 22:40:47 +0100

    Actualización NUnit3 + Renombrar
    Se actualiza el estilo de los tests a NUnit3, con un datasource para test parametrizado
    Como ahora es parametrizado, no depende de probar solo el Simple. El proyecto de test se ha renombrado a genérico y sirve para cualquier algoritmo.
    Se ajustan los namespaces tras renombrar el proyecto

---
+++ TestDijkstraAlgorithm/TestAlgorithm.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using DijkstraAlgorithm;
+using NUnit.Framework;
+
+namespace TestDijkstraAlgorithm
+{
+    [TestFixture]
+    public class TestAlgorithm
+    {
+        #region Methods
+
+        [Test, TestCaseSource(typeof(TestAlgorithmSource), nameof(TestAlgorithmSource.BuildTestCases))]
+        public void TestDijkstraAlgorithm(Graph graph, Vertex from, Vertex to, IEnumerable<Vertex> expected)
+        {
+            var calculatedPath = graph.ShortestPath(from, to).GetPathToDestination();
+            Assert.That(calculatedPath, Is.EqualTo(expected), $"Path from {from.Name} to {to.Name} miscalculated");
+        }
+
+        #endregion Methods
+    }
+}
---
+++ TestDijkstraAlgorithm/TestAlgorithmSource.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using DijkstraAlgorithm;
+using NUnit.Framework;
+
+namespace TestDijkstraAlgorithm
+{
+    public class TestAlgorithmSource
+    {
+        public static IEnumerable<TestCaseData> BuildTestCases()
+        {
+            (var vertexes, var edges) = BuildGraph();
+            foreach (var algorithm in DijkstraAlgorithms)
+            {
+                var graph = new Graph(algorithm, vertexes, edges);
+                yield return new TestCaseData(graph, vertexes[2], vertexes[0], new[] { vertexes[2], vertexes[0] });
+                yield return new TestCaseData(graph, vertexes[2], vertexes[4], new[] { vertexes[2], vertexes[0], vertexes[1], vertexes[4] });
+            }
+        }
+
+        private static IEnumerable<IDijkstraAlgorithm> DijkstraAlgorithms => new[]
+        {
+            new SimpleDijkstraAlgorithm.SimpleDijkstraAlgorithm(),
+        };
+
+        private static (Vertex[] vertexes, Edge[] edges) BuildGraph()
+        {
+            var vertexes = new[]
+            {
+                new Vertex("A"),
+                new Vertex("B"),
+                new Vertex("C"),
+                new Vertex("D"),
+                new Vertex("E"),
+            };
+            var edges = new[]
+            {
+                new Edge(vertexes[0], vertexes[1], 1),
+                new Edge(vertexes[0], vertexes[2], 3),
+                new Edge(vertexes[1], vertexes[2], 7),
+                new Edge(vertexes[1], vertexes[4], 1),
+                new Edge(vertexes[1], vertexes[3], 2),
+                new Edge(vertexes[2], vertexes[3], 5),
+                new Edge(vertexes[3], vertexes[4], 3),
+            };
+            return (vertexes, edges);
+        }
+    }
+}
